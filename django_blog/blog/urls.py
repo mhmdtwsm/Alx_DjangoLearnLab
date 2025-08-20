eb6611e372170f06cb8e8b373996d7210dbd5ea7
mhmdtwsm <sherefmohamed93@gmail.com>
2025-08-20 11:51:30 +0300

    123134

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,15 +10,35 @@ from .views import (
     CommentDeleteView,
 )
 
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # Third-party apps
+    "taggit",
+    # Your apps
+    "blog",
+]
+
 urlpatterns = [
     path("", PostListView.as_view(), name="post-list"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-
     # Comment URLs
-    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-create"),
-    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
-    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+    path(
+        "post/<int:pk>/comments/new/",
+        CommentCreateView.as_view(),
+        name="comment-create",
+    ),
+    path(
+        "comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"
+    ),
+    path(
+        "comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"
+    ),
 ]
