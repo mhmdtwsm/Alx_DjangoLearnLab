d341eacbdacfbf14abdbd23192541826a9191b58
mhmdtwsm <sherefmohamed93@gmail.com>
2025-08-20 11:54:45 +0300

    123311331

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,10 +14,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # third-party apps
-    "taggit",
-    # local apps
     "blog",  # our app
+    "taggit",  # tagging functionality
 ]
 
 MIDDLEWARE = [
