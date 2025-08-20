7e5f900bdd9e44c178d6e95460ed592d6fc77643
mhmdtwsm <sherefmohamed93@gmail.com>
2025-08-20 11:09:59 +0300

    tyty

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,16 +1,22 @@
 from rest_framework import generics, filters
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as django_filters
 from .models import Book
 from .serializers import BookSerializer
 from .filters import BookFilter
 
+
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [
+        django_filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
     filterset_class = BookFilter
-    search_fields = ['title', 'author']
-    ordering_fields = ['title', 'publication_year']
+    search_fields = ["title", "author"]
+    ordering_fields = ["title", "publication_year"]
+
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
