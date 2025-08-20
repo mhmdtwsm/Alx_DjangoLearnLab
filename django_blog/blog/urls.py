558fd51ad40015bdbb1430347fd66eeb7c08dac8
mhmdtwsm <sherefmohamed93@gmail.com>
2025-08-20 11:33:53 +0300

    qwerttttt

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("register/", views.register, name="register"),
-    path("login/", views.UserLoginView.as_view(), name="login"),
-    path("logout/", views.UserLogoutView.as_view(), name="logout"),
-    path("profile/", views.profile, name="profile"),
+    path("", PostListView.as_view(), name="post-list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
